1455044779d2463d87137b757a84353e82bee488
djkarts <kartiksingla@gmail.com>
2013-08-10 21:42:02 +0000

    change 3.2

--- web.js
+++ web.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buffer = require ('buffer');
-  response.send(buffer.toString(fs.readFileSync("index.html")));
+  response.send(fs.readFileSync("index.html"));
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
