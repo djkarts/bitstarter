9dc77100c07b55d7d0b892a60af7d6644f6ac99f
djkarts <kartiksingla@gmail.com>
2013-08-10 21:13:20 +0000

    added buffer functions

--- web.js
+++ web.js
@@ -1,19 +1,13 @@
-// var express = require('express');
-// var app = express();
-// app.use(express.logger());
+ var express = require('express');
+ var app = express();
+ app.use(express.logger());
 
-// app.get('/', function(request, response) {
-//   response.send('Hello World 2!');
-// });
+ app.get('/', function(request, response) {
+   response.send(fs.readFileSync(buf.toString("index.html")));
+ });
 
-// var port = process.env.PORT || 5000;
-// app.listen(port, function() {
-//   console.log("Listening on " + port);
-// });
-<html>
-<body>
+ var port = process.env.PORT || 5000;
+ app.listen(port, function() {
+   console.log("Listening on " + port);
+ });
 
-Hello world from index.html
-
-</html>
-</body>
